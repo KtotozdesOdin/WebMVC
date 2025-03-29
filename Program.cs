00baf2186aedc51f6cd748a88f895a193cca5b4d
KtotozdesOdin <se.me.nov007@gmail.com>
2025-03-29 17:43:56 +0300

    Упр.2. Добавлена папка wwwroot, файл стилей CSS. В program.cs строка для работы со статическими файлами.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace WebMVCR1
             builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
+            //Строка для работы со статическими файлами
+            app.UseStaticFiles();
+
             app.MapControllerRoute(
                  name: "default",
                  pattern: "{controller=Home}/{action=Index}/{id?}");
